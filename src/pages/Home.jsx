33a0a765140977476f224cbeedfb31a4bf73d9bd
eadarak00 <eadarak2001@gmail.com>
2025-09-29 22:19:28 +0000

    feat: creation de la page d'accueil

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import MainHero from "../components/MainHero";
+import Stack from "../components/Stack";
+
+export default function HomePage() {
+
+  return (
+    <main className="main-hero">
+      <MainHero/>
+      <Stack/>
+    </main>
+  );
+}
